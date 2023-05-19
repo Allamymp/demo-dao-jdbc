71196bc53679f7864a926ee970c126e2773ba7d7
allamymp <allamympereira@gmail.com>
2023-05-19 06:57:12 -0300

    delete() validation parameter implemented

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -85,7 +85,10 @@ public class SellerDaoJDBC implements SellerDao {
         try{
             st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
             st.setInt(1,id);
-            st.executeUpdate();
+            int rows = st.executeUpdate();
+            if(rows == 0 ){
+                throw  new SQLException();
+            }
         }
         catch (SQLException e){
             throw new DbException(e.getMessage());
