f477b687f2d42d8727c9b81506de752cf84cbe3c
allamymp <allamympereira@gmail.com>
2023-05-19 06:57:29 -0300

    delete() implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,17 +27,21 @@ public class Program {
         for (Seller sellerCaptured : listAll) {
             System.out.println("Seller: " + sellerCaptured);
         }
-        System.out.println("\n=== TEST 4: seller insert ===");
+       /* System.out.println("\n=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.00,
                 new Department(2,null ));
         sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = "+ newSeller.getId());
+        System.out.println("Inserted! New id = "+ newSeller.getId()); */
         System.out.println("\n=== TEST 5: seller update ===");
          seller = sellerDao.findById(1);
          seller.setName("Marta Wayne");
          seller.setEmail("martaWayne@gmail.com");
          sellerDao.update(seller);
          System.out.println("Update completed");
+        System.out.println("\n=== TEST 6: seller update ===");
+        int id = 9;
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed!");
 
     }
 }
