97a32483dc8b7dbcd41a04b6c44fc95c16ddb197
allamymp <allamympereira@gmail.com>
2023-05-19 07:52:58 -0300

    update() tested

--- src/application/program2.java
+++ src/application/program2.java
@@ -25,18 +25,18 @@ public class program2 {
                 for (Department d : list) {
                     System.out.println(d);
                 }
-
+/*
                 System.out.println("\n=== TEST 3: insert =======");
                 Department newDepartment = new Department(null, "Music");
                 departmentDao.insert(newDepartment);
                 System.out.println("Inserted! New id: " + newDepartment.getId());
-/*
+*/
                 System.out.println("\n=== TEST 4: update =======");
                 Department dep2 = departmentDao.findById(1);
                 dep2.setName("Food");
                 departmentDao.update(dep2);
                 System.out.println("Update completed");
-
+/*
                 System.out.println("\n=== TEST 5: delete =======");
                 System.out.print("Enter id for delete test: ");
                 int id = sc.nextInt();
